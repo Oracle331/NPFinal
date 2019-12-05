cc136c786bf90ed37b022f028dd1f212a23e681f
Anthony J. Salaris <salarisa1@wit.edu>
2019-12-04 21:31:51 -0500

    v5

--- client.py
+++ client.py
@@ -2,12 +2,21 @@ from socket import *
 import sys
 import threading
 
-host = "10.220.82.40"  # 127.0.0.1
+# @authors Andrew Galvin, Endi Caushi, Anthony Salaris
+# @version 1.0.0 - Simple client per class assignment
+# @version 2.0.0 - Added constant send/receive messages from other clients
+
+# the server IP is manually set here for the local host
+host = "127.0.0.1"  # IP can be found in cmd prompt by typing 'ipconfig'
 port = 5089
 
 
+# Creates a new thread when a user connects
 class listen(threading.Thread):
+    # Method to initialize the thread for the client
+
     def __init__(self, client):
+        # create a new thread
         threading.Thread.__init__(self)
         self.client = client
         self.setDaemon(True)
@@ -29,28 +38,32 @@ class listen(threading.Thread):
                 print("You have disconnected")
 
 
+# Main module to run everything
 if __name__ == "__main__":
+    # initialize TCP socket #
     clientSocket = socket(AF_INET, SOCK_STREAM)
+    # connect the socket to the given host and port #
     clientSocket.connect((host, port))
+    # display welcome messsage to client #
     try:
-        print("Welcome to the chat!")
-        print("Type out a message and press 'Enter' to send it")
-        print("Use '/name' command to change your username.")
-        print("Use '/quit' command to quit.")
+        print("----------Welcome to our chat-----------------------------------")
+        print("----------Type your message and press 'Enter' to send.----------")
+        print("----------Send '/name' command to change your username.---------")
+        print("----------Send '/quit' command to quit.-------------------------")
     except error as e:
+        # if unable to open a socket #
         if clientSocket:
-            clientSocket.close()
-        print("Could not open a socket: " + str(e))
+            # close the socket #
+            socket.close(clientSocket)
+        # error message to print out if unable to open  the socket #
+        print("Unable to open a socket: " + str(e))
+        # shutdown the client #
         sys.exit(1)
 
-    l = listen(clientSocket)
-    l.start()
+    connection = listen(clientSocket)
+    connection.start()
     message = input()
     while message != "/quit":
-        # sys.stdout.flush()
         clientSocket.send(message.encode())
-        # data = self.clientSocket.recv(1024)
-        # data = data.decode()
-        # print("Recieved: "+str(data))
         message = input()
     clientSocket.close()
--- server.py
+++ server.py
@@ -4,34 +4,43 @@ import sys
 import threading
 import time
 
-QUIT = False
+
+# @authors Andrew Galvin, Endi Caushi, Anthony Salaris
+# @version 1.0.0 - Simple client per class assignment
+# @version 2.0.0 - Adjusted for multiple clients
 
 
+# Manage threads per user
 class Server:
+
+    # declare variables
     def __init__(self):
-        self.host = "10.220.82.40"
+
+        # manually set server on local host
+        self.host = "127.0.0.1"
         self.port = 5089
         self.threads = []
         self.backlog = 10
 
+    # start and run the server
     def run(self):
-
         while True:
+            # start the server if the port is available
             try:
-                all_good = False
-                self.sock = socket(AF_INET, SOCK_STREAM)
-                self.sock.bind((self.host, self.port))
-                self.sock.listen(self.backlog)
+                user = socket(AF_INET, SOCK_STREAM)
+                user.bind((self.host, self.port))
+                user.listen(self.backlog)
 
                 print("Server started at " + self.host + ":" + str(self.port))
                 break
             except Exception as err:
                 print('Socket connection error... ')
-                self.sock.close()
+                self.user.close()
         try:
+            # create a new thread for each user  that connects
             while True:
                 try:
-                    client, addr = self.sock.accept()
+                    client, addr = self.user.accept()
                 except socket.timeout:
                     continue
                 new_thread = Client(client)
@@ -41,14 +50,15 @@ class Server:
                 for each in self.threads:
                     each.client.send(msg)
 
+                # add the user to the array of threads
                 self.threads.append(new_thread)
                 new_thread.start()
 
+                # remove inactive threads
                 for thread in self.threads:
                     if not thread.is_alive():
                         self.threads.remove(thread)
                         thread.join()
-                        # time.sleep(1)
         except KeyboardInterrupt:
             print("Terminating by Ctrl+C")
         except Exception as err:
@@ -58,8 +68,10 @@ class Server:
         self.sock.close()
 
 
+# permissions for clients connecting
 class Client(threading.Thread):
 
+    # declare variables
     def __init__(self, client):
         threading.Thread.__init__(self)
         self.client = client
@@ -67,11 +79,13 @@ class Client(threading.Thread):
 
         self.done = False
 
+    # hold a connection with the client
     def run(self):
-
         while not self.done:
+            # if a command is received
             try:
                 cmd = self.client.recv(1024).decode()
+                # change username
                 if cmd.startswith("/name"):
                     self.client.send("Enter your username: ".encode())
                     old_name = self.name
@@ -81,17 +95,22 @@ class Client(threading.Thread):
                         if each != self and each.is_alive():
                             each.client.send(msg.encode())
                     self.client.send(("Your username has been changed to %s" % self.name).encode())
+                # disconnect from server
                 elif cmd == "/quit":
                     self.remove()
-                elif cmd.startswith("/filetransfer"):
-                    self.client.send("Please enter the filename of the file: ".encode())
-                    filename = self.client.recv(1024).decode()
-                    file = open(filename, 'rb')
-                    file_data = file.read(4096)
-                    for each in server.threads:
-                        if each != self and each.is_alive():
-                            self.fileTransfer(each)
-                    print("Data has been transmitted successfully")
+                    # below is the attempt at file transfer
+                # elif cmd.startswith("/filetransfer"):
+                #     self.client.send("Please enter the filename of the file: ")
+                #     filename = self.client.recv(1024)
+                #     file = open(filename, 'rb')
+                #     file_data = file.read(4096).decode()
+                #
+                #     for each in server.threads:
+                #         if each != self and each.is_alive():
+                #             each.fileTransfer(file_data)
+                #     print("Data has been transmitted successfully")
+
+                # send message
                 else:
                     msg = "%s: %s" % (self.name, cmd)
                     for each in server.threads:
@@ -99,9 +118,9 @@ class Client(threading.Thread):
                             each.client.send(msg.encode())
             except Exception as e:
                 print("Connection lost", e)
-                # self.remove()
                 break
-                # continue
+
+        # user disconnects, remove thread
         server.threads.remove(self)
         self.client.close()
         return
@@ -109,22 +128,24 @@ class Client(threading.Thread):
     def remove(self):
         self.client.send("exit".encode())
         server.threads.remove(self)
-        QUIT = True
         self.done = True
+    # attempt at file transfer
+    # def fileTransfer(self, file_data):
+    #     # filename = input(str("Please enter a filename for the incoming file: "))
+    #     # user.client.send("Please enter a filename for the incoming file: ".encode())
+    #     filename = self.client.recv(1024).decode()
+    #     # file_data = self.client.recv(4096).decode()
+    #
+    #     # with open(filename, 'wb') as file:
+    #     file = open(filename, 'wb')
+    #     # file.write(file_data)
+    #     self.client.send(file_data)
+    #
+    #     file.close()
+    #     print("File has been received successfully.")
 
-    def fileTransfer(self, user):
-        # filename = input(str("Please enter a filename for the incoming file: "))
-        user.client.send("Please enter a filename for the incoming file: ".encode())
-        filename = user.client.recv(1024).decode()
-        with open(filename, 'wb') as file:
-            file_data = user.client.recv(4096)
-            file.write(file_data)
-        # file = open(filename, 'wb')
 
-        # file.close()
-        print("File has been received successfully.")
-
-
+# run server
 if __name__ == "__main__":
     server = Server()
     server.run()
